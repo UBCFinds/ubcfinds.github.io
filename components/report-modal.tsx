36d57f1c93b0d09f503b66e1d5804005af01ccef
Anant Khanna <itsanantk@gmail.com>
2025-10-27 18:07:55 -0700

    updated env var pull

--- components/report-modal.tsx
+++ components/report-modal.tsx
@@ -14,7 +14,7 @@ import { createClient } from '@supabase/supabase-js';
 //import { supabase } from "@/supabaseClient"; // Import the Supabase client
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or Key is missing in environment variables.");
